59bb7581cce14f18d087bf40bf82c72b6caf62ef
knh6269 <knh6269@gmail.com>
2022-11-09 14:30:50 +0900

    feat(signUp): 회원가입 api 연동
    회원가입 api 연동 코드 작성

--- peacock/src/api.js
+++ peacock/src/api.js
@@ -1,2 +1,12 @@
 const URL =" http://43.200.68.165:3000";
 
+export const postSignup = async (data) => {
+    return fetch(`${URL}/user`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json());
+  }
