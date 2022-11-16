e975dad45ed737a64f953cd0808ad9f8f61de3f5
knh6269 <knh6269@gmail.com>
2022-11-17 02:14:43 +0900

    feat(Router): 라우터 수정

--- peacock/src/Router.js
+++ peacock/src/Router.js
@@ -1,16 +1,15 @@
 import {BrowserRouter,useParams,Routes,Route, useNavigate} from "react-router-dom";
-import Closet from "./routes/Closet";
+import Closet from "./routes/Closet/Closet";
 import Main from "./routes/Main";
 import Ranking from "./routes/Ranking";
-import Layout from "./Layout";
-import Dictionary from "./routes/Dictionary";
-import LogIn from "./routes/LogIn";
-import SignUp from "./routes/SingUp";
+import Layout from "./routes/Layout";
+import Dictionary from "./routes/Dictionary/Dictionary";
+import LogIn from "./routes/LoginSignUp/LogIn";
+import SignUp from "./routes/LoginSignUp/SingUp";
 import Search from "./routes/Search";
 import MyPageLayout from "./Mypage/MypageLayout";
 import MyPageCloset from "./Mypage/MyPageCloset";
 import MyPageFollower from "./Mypage/MypageFollower";
-import MyPageSetting from "./Mypage/MyPageSetting";
 import MyPageStyleFix from "./Mypage/MypageStyleFix";
 import MyPageInfoFix  from "./Mypage/MyPageInfoFix";
 
@@ -20,7 +19,7 @@ function Router(){
         <Routes>
             <Route element={<Layout />}>
             <Route path="/" element={<Main/>}></Route>
-            <Route path="/closet/:username" element={<Closet/>}></Route>
+            <Route path="/closet/:userid" element={<Closet/>}></Route>
             <Route path="/ranking" element={<Ranking/>}></Route>
             <Route path="/dictionary" element={<Dictionary/>}></Route>
             <Route path="/search/:keyword" element={<Search/>}></Route>
@@ -28,12 +27,10 @@ function Router(){
                 <Route path="/myPage/myPageInfoFix" element={<MyPageInfoFix/>}></Route>
                 <Route path="/myPage/myPageCloset" element={<MyPageCloset/>}></Route>
                 <Route path="/myPage/myPageFollower" element={<MyPageFollower/>}></Route>
-                <Route path="/myPage/myPageSetting" element={<MyPageSetting/>}></Route>
                 <Route path="/myPage/myPageStyleFix" element={<MyPageStyleFix/>}></Route>
             </Route>
             <Route path="/logIn" element={<LogIn/>}></Route>
             <Route path="/signUp" element={<SignUp/>}></Route>
-            
             </Route>
         </Routes>
     </BrowserRouter>
