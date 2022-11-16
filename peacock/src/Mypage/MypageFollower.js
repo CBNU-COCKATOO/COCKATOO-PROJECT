2777372c63db53b523c35438c7ee6bf02fb35982
knh6269 <knh6269@gmail.com>
2022-11-17 02:15:08 +0900

    fix(MypageFollower): 마이페이지 팔로우 관리 페이지 수정

--- peacock/src/Mypage/MypageFollower.js
+++ peacock/src/Mypage/MypageFollower.js
@@ -1,33 +1,81 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components"
-import {Link,useNavigate,Outlet} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { TotalId } from "../Recoil/Atoms";
+import Slider from "react-slick";
+import '../Style/slick.css';
+import "../Style/slick-theme.css";
+import '../index.css'
+import { getRanking, getFollowingCloset, setFollowingCloset } from '../Recoil/Api';
+import { useMutation } from "react-query";
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
     text-align: center;
     flex-direction: column;
     height:70vh;
+    padding-top:3vh;
+    border-radius:1vw;
 `;
 
-const Logo = styled.div`
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-    h1{
-        font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-        margin-left: 1.5vw;
+const FollowingProfile = styled.div`
+    display: flex;
+    gap: 1vw;
+    margin-top:1vh;
+    flex-wrap:wrap;
+    margin-left:2vw;
+    .followingProfiles{
+        width:12vw;
+        height:12vw;
+        border-radius: 50%;
+        z-index: 1;
+        filter: brightness(50%);
+        cursor: pointer;
+    }
+    .profiles{
+        justify-content: center;
+        align-items: center;
+        text-align:center;
     }
 `;
 
+const Name = styled.div`
+    position:relative;
+    color:#F6F1FF;
+    top:11vh;
+    right:7.5vw;
+    font-size:1.1vw;
+    z-index: 2; 
+`;
 
 function MyPageFollower(){
+    const [follow, setFollow] = useState([]);
+    const id = useRecoilValue(TotalId);
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        getFollowingCloset(id)
+        .then(response => response.json())
+        .then(response => {
+            setFollow(response)
+        })
+    },[follow])
+
+    const onClickTodayRanking = (u_id) => {
+        navigate(`/closet/${u_id}`)
+    }
     return(
         <Container>
-             <Logo>
-                <h1 >팔로우 관리</h1>
-             </Logo>
+            <FollowingProfile>
+                {follow.length > 0 && follow.map((item,index)=>{
+                            return(
+                                     <div onClick = {() => onClickTodayRanking(item.u_id)} key={index}style={{display:"flex",textAlign:"center"}}>
+                                        <img src={item.u_image} alt="profilesImg" className="followingProfiles"/>
+                                        <Name>{item.u_name}</Name>
+                                      </div>
+                            )
+                })}
+            </FollowingProfile>
         </Container>    
         )
 }
